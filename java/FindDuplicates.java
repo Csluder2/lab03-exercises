c4b8243d9654a153e4401775fa5d9570fd355ed9
root <root@ChristiansCPU>
2024-09-11 00:35:06 -0400

    Hashmap method implemented in FindDuplicates

--- java/FindDuplicates.java
+++ java/FindDuplicates.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.*;
 public class FindDuplicates {
 
 	public static List<Integer> findModeNestedLoops(List<Integer> test) {
@@ -20,16 +20,46 @@ public class FindDuplicates {
 		return comparison;
 	}
 
-	public static void main(String[] args) {
-		// some test strings:
-		List<Integer> sample1 = new ArrayList<Integer>(Arrays.asList(3, 7, 5, 6, 7, 4, 8, 5, 7, 66));
-		List<Integer> sample2 = new ArrayList<Integer>(Arrays.asList(3, 5, 6, 4, 4, 5, 66, 6, 7, 6));
-		List<Integer> sample3 = new ArrayList<Integer>(Arrays.asList(3, 0, 5, 1, 0));
-		List<Integer> sample4 = new ArrayList<Integer>(Arrays.asList(3));
-		System.out.println("Sample 1: " + findModeNestedLoops(sample1));
-		System.out.println("Sample 2: " + findModeNestedLoops(sample2));
-		System.out.println("Sample 3: " + findModeNestedLoops(sample3));
-		System.out.println("Sample 4: " + findModeNestedLoops(sample4));
-	}
+    public static List<Integer> findDuplicatesWithHashMap(List<Integer> test) {
+        Map<Integer, Integer> comparisonMap = new HashMap<>();
+    List<Integer> comparison = new ArrayList<>();
 
+    
+    for (int i = 0; i < test.size(); i++) {
+        Integer num = test.get(i); 
+        if (comparisonMap.containsKey(num)) {
+            comparisonMap.put(num, comparisonMap.get(num) + 1);
+        } else {
+            comparisonMap.put(num, 1);
+        }
+    }
+
+    
+    for (int i = 0; i < test.size(); i++) {
+        Integer num = test.get(i); 
+        if (comparisonMap.get(num) > 1 && !comparison.contains(num)) {
+            comparison.add(num);
+        }
+    }
+
+    return comparison;
+}
+
+    public static void main(String[] args) {
+
+        List<Integer> sample1 = new ArrayList<>(Arrays.asList(3, 7, 5, 6, 7, 4, 8, 5, 7, 66));
+        List<Integer> sample2 = new ArrayList<>(Arrays.asList(3, 5, 6, 4, 4, 5, 66, 6, 7, 6));
+        List<Integer> sample3 = new ArrayList<>(Arrays.asList(3, 0, 5, 1, 0));
+        List<Integer> sample4 = new ArrayList<>(Arrays.asList(3));
+
+        System.out.println("Sample 1: " + findModeNestedLoops(sample1));
+        System.out.println("Sample 2: " + findModeNestedLoops(sample2));
+        System.out.println("Sample 3: " + findModeNestedLoops(sample3));
+        System.out.println("Sample 4: " + findModeNestedLoops(sample4));
+
+        System.out.println("Sample 1 Hashmap: " + findDuplicatesWithHashMap(sample1));
+        System.out.println("Sample 2 Hashmap: " + findDuplicatesWithHashMap(sample2));
+        System.out.println("Sample 3 Hashmap: " + findDuplicatesWithHashMap(sample3));
+        System.out.println("Sample 4 Hashmap: " + findDuplicatesWithHashMap(sample4));
+    }
 }
