007bd412a83ef400b116236f40bf05eb9dbd9ff3
root <root@ChristiansCPU>
2024-09-10 23:45:21 -0400

    find_duplicates.py finished

---
+++ python/find_duplicates.py
@@ -0,0 +1,18 @@
+def find_duplicates_nested_loop(test: list) -> list:
+    comparison = []
+    for i in range(len(test)):
+     for j in range (i + 1, len(test)):
+       if test[i] == test[j] and test[i] not in comparison:
+                comparison.append(test[i])
+    return comparison
+
+if __name__ == "__main__":
+    sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
+    sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
+    sample3 = [3, 0, 5, 1, 0]
+    sample4 = [3]
+    
+    print("Sample 1:", find_duplicates_nested_loop(sample1))
+    print("Sample 2:", find_duplicates_nested_loop(sample2))
+    print("Sample 3:", find_duplicates_nested_loop(sample3))
+    print("Sample 4:", find_duplicates_nested_loop(sample4))
